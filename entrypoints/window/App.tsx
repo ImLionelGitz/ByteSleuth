e8da87364cef125fa86337754e38facf1b24698a
ImLionelGitz <90650385+ImLionelGitz@users.noreply.github.com>
2026-05-08 16:25:47 +0600

    second wxt version

--- entrypoints/window/App.tsx
+++ entrypoints/window/App.tsx
@@ -1,112 +1,89 @@
-import { sendToContentJS } from '@/helpers/messager'
-import { pinger } from '@/helpers/pinger'
-import { Box, Dialog, Stack } from '@mui/material'
-import PlayButton from './components/PlayButton'
-import TableFieldUI from './interfaces/GreenUI'
-import TablePanel from './interfaces/PurpleUI'
+// import { sendToContentJS } from '@/helpers/messager'
+// import { pinger } from '@/helpers/pinger'
+import TablePanel from './interfaces/TablePanel'
 
 function App() {
-   const [isDialogOpen, setDialogOpen] = useState(false)
-   const [fieldUI, setField] = useState<FieldByte[]>([])
+   // const [isDialogOpen, setDialogOpen] = useState(false)
+   // const [fieldUI, setField] = useState<FieldByte[]>([])
 
-   async function handleClose() {
-      const [tab] = await browser.tabs.query({
-         active: true,
-         currentWindow: false,
-      })
+   // async function handleClose() {
+   //    const [tab] = await browser.tabs.query({
+   //       active: true,
+   //       currentWindow: false,
+   //    })
 
-      if (tab.id) {
-         sendToContentJS(tab.id, { message: 'selection cancelled' })
-      }
+   //    if (tab.id) {
+   //       sendToContentJS(tab.id, { message: 'selection cancelled' })
+   //    }
 
-      setDialogOpen(false)
-   }
+   //    setDialogOpen(false)
+   // }
 
-   async function handlePlay() {
-      const [tab] = await browser.tabs.query({
-         active: true,
-         currentWindow: false,
-      })
+   // async function handlePlay() {
+   //    const [tab] = await browser.tabs.query({
+   //       active: true,
+   //       currentWindow: false,
+   //    })
 
-      if (tab.id) {
-         sendToContentJS(tab.id, { message: 'begin scrape', list: fieldUI })
-      }
-   }
+   //    if (tab.id) {
+   //       sendToContentJS(tab.id, { message: 'begin scrape', list: fieldUI })
+   //    }
+   // }
 
-   function handleFieldAdd(type: FieldTypes) {
-      setField((old) => {
-         const newField: FieldByte = {
-            id: old.length,
-            type: type,
-            name: 'New Field',
-            selector: '',
-         }
+   // function handleFieldAdd(type: FieldTypes) {
+   //    setField((old) => {
+   //       const newField: FieldByte = {
+   //          id: old.length,
+   //          type: type,
+   //          name: 'New Field',
+   //          selector: '',
+   //       }
 
-         return [...old, newField]
-      })
-   }
+   //       return [...old, newField]
+   //    })
+   // }
 
-   function handleFieldUpdate(newField: FieldByte) {
-      setField((old) =>
-         old.map((oldField) => {
-            if (oldField.id === newField.id) {
-               return newField
-            }
+   // function handleFieldUpdate(newField: FieldByte) {
+   //    setField((old) =>
+   //       old.map((oldField) => {
+   //          if (oldField.id === newField.id) {
+   //             return newField
+   //          }
 
-            return oldField
-         })
-      )
+   //          return oldField
+   //       })
+   //    )
 
-      console.log(fieldUI)
-   }
+   //    console.log(fieldUI)
+   // }
 
-   function handleFieldDelete(id: number) {
-      setField((old) => {
-         const raw = old.filter((oldField) => oldField.id !== id)
-         return raw.map((field, i) => ({ ...field, id: i }))
-      })
-   }
+   // function handleFieldDelete(id: number) {
+   //    setField((old) => {
+   //       const raw = old.filter((oldField) => oldField.id !== id)
+   //       return raw.map((field, i) => ({ ...field, id: i }))
+   //    })
+   // }
 
-   useEffect(() => {
-      pinger((msg) => {
-         switch (msg.message) {
-            case 'selection ongoing':
-               setDialogOpen(true)
-               break
+   // useEffect(() => {
+   //    pinger((msg) => {
+   //       switch (msg.message) {
+   //          case 'selection ongoing':
+   //             setDialogOpen(true)
+   //             break
 
-            case 'selection done':
-               setDialogOpen(false)
-               break
+   //          case 'selection done':
+   //             setDialogOpen(false)
+   //             break
 
-            default:
-               break
-         }
-      })
-   }, [])
+   //          default:
+   //             break
+   //       }
+   //    })
+   // }, [])
 
    return (
       <div>
-         <Stack
-            direction="row"
-            sx={{ alignItems: 'center', justifyContent: 'center' }}
-         >
-            <TablePanel scale={0.9} />
-
-            <Stack direction="column" sx={{ alignItems: 'center', gap: 0.8 }}>
-               <TableFieldUI
-                  fieldList={fieldUI}
-                  fieldAdd={handleFieldAdd}
-                  fieldUpdate={handleFieldUpdate}
-                  fieldDelete={handleFieldDelete}
-               />
-
-               <PlayButton onClick={handlePlay} />
-            </Stack>
-         </Stack>
-
-         <Dialog open={isDialogOpen} onClose={handleClose}>
-            <Box>lol</Box>
-         </Dialog>
+         <TablePanel />
       </div>
    )
 }
---
+++ entrypoints/window/interfaces/TablePanel.tsx
@@ -0,0 +1,10 @@
+import Panel from '@/assets/Panel.png'
+import { Paper } from '@mui/material'
+
+export default function TablePanel() {
+   return (
+      <div>
+         <Paper className="w-[200px]">lol</Paper>
+      </div>
+   )
+}
